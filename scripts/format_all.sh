a03fdf8c90a7497c804bf9dc1831341bbd46f056
Mike <90835468+justmike1@users.noreply.github.com>
2022-12-07 15:50:58 +0200

    Update format_all.sh

--- scripts/format_all.sh
+++ scripts/format_all.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+success=0
+
 p_args=(--write)
 p_log="Running prettier"
-
 log="Running black & isort"
+
 if [[ "$1" = "--check" || "$1" = "-c" ]]; then
 	log="$log in check only mode"
 	p_log="$p_log in check only mode"
@@ -19,3 +21,9 @@ isort . "${args[@]}"
 echo "$p_log..."
 
 npx prettier . "${p_args[@]}"
+
+if [ $? -ne 0 ]; then
+	success=1
+fi
+
+exit $success
